22b8f816b66010a844bb90ae75833a3afb77097d
CristinaCornatanu <cristinaangela.cornatanu@ulbsibiu.ro>
2023-11-25 15:03:09 +0200

    test

--- ShopOnline/Repository/OrderDetailsRepository.cs
+++ ShopOnline/Repository/OrderDetailsRepository.cs
@@ -1,7 +1,104 @@
 ﻿
+using ShopOnline.Data;
+using ShopOnline.Models.DBObjects;
+using ShopOnline.Models;
+
 namespace ShopOnline.Repository
 {
     public class OrderDetailsRepository
     {
+
+        private readonly ApplicationDbContext dbContext;
+
+        public OrderDetailsRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public List<OrderDetailsModel> GetAllOrderDetails()
+        {
+
+            List<OrderDetailsModel> orderList = new List<OrderDetailsModel>();
+            foreach (OrderDetail dbcat in this.dbContext.OrderDetails)
+            {
+                orderList.Add(MapDbObjectToModel(dbcat));
+            }
+            return orderList;
+
+        }
+
+        public OrderDetailsModel GetOrderDetailsById(Guid id)
+        {
+            return MapDbObjectToModel(dbContext.OrderDetails.FirstOrDefault(x => x.IdOrderDetails == id));
+        }
+
+        public void InsertOrderDetails(OrderDetailsModel orderModel)
+        {
+            orderModel.IdOrder = Guid.NewGuid();
+            dbContext.OrderDetails.Add(MapModelToDbObject(orderModel));
+            dbContext.SaveChanges();
+        }
+
+        public void UpdateOrderDetails(OrderDetailsModel orderModel)
+        {
+            OrderDetail order = dbContext.OrderDetails.FirstOrDefault(x => x.IdOrderDetails == orderModel.IdOrderDetails);
+            if (order != null)
+            {
+                order.IdOrderDetails = orderModel.IdOrderDetails;
+                order.IdOrder = orderModel.IdOrder;
+                order.IdProduct = orderModel.IdProduct;
+                order.IdTva = orderModel.IdTva;
+                order.PriceTva = orderModel.PriceTva;
+                order.Quantity = orderModel.Quantity;
+                order.PriceTotal = orderModel.PriceTotal;
+            }
+            dbContext.SaveChanges();
+        }
+
+        public void DeleteOrderDetails(Guid id)
+        {
+            var existingOrder = dbContext.OrderDetails.FirstOrDefault(c => c.IdOrderDetails == id);
+            if (existingOrder != null)
+            {
+                dbContext.OrderDetails.Remove(existingOrder);
+
+            }
+            dbContext.SaveChanges();
+        }
+
+        private OrderDetailsModel MapDbObjectToModel(OrderDetail dbOrder)
+        {
+            OrderDetailsModel ordermodel = new OrderDetailsModel();
+            if (dbOrder != null)
+            {
+                
+                ordermodel.IdOrderDetails = dbOrder.IdOrderDetails;
+                ordermodel.IdOrder = dbOrder.IdOrder;
+                ordermodel.IdProduct = dbOrder.IdProduct;
+                ordermodel.IdTva = dbOrder.IdTva;
+                ordermodel.PriceTva = dbOrder.PriceTva;
+                ordermodel.Quantity = dbOrder.Quantity;
+                ordermodel.PriceTotal = dbOrder.PriceTotal;
+            }
+            return ordermodel;
+        }
+
+        private OrderDetail MapModelToDbObject(OrderDetailsModel orderModel)
+        {
+
+            OrderDetail dbOrder = new OrderDetail();
+            if (dbOrder != null)
+            {
+                dbOrder.IdOrderDetails = orderModel.IdOrderDetails;
+                dbOrder.IdOrder = orderModel.IdOrder;
+                dbOrder.IdProduct = orderModel.IdProduct;
+                dbOrder.IdTva = orderModel.IdTva;
+                dbOrder.PriceTva = orderModel.PriceTva;
+                dbOrder.Quantity = orderModel.Quantity;
+                dbOrder.PriceTotal = orderModel.PriceTotal;
+            }
+            return dbOrder;
+        }
+
     }
 }
--- ShopOnline/Repository/OrderRepository.cs
+++ ShopOnline/Repository/OrderRepository.cs
@@ -1,7 +1,99 @@
-﻿namespace ShopOnline.Repository
+﻿using ShopOnline.Data;
+using ShopOnline.Models;
+using ShopOnline.Models.DBObjects;
+
+namespace ShopOnline.Repository
 {
     public class OrderRepository
     {
+
+        private readonly ApplicationDbContext dbContext;
+
+        public OrderRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public List<OrderModel> GetAllOrders()
+        {
+
+            List<OrderModel> orderList = new List<OrderModel>();
+            foreach (Order dbcat in this.dbContext.Orders)
+            {
+                orderList.Add(MapDbObjectToModel(dbcat));
+            }
+            return orderList;
+
+        }
+
+        public OrderModel GetOrderById(Guid id)
+        {
+            return MapDbObjectToModel(dbContext.Orders.FirstOrDefault(x => x.IdOrder == id));
+        }
+
+        public void InsertOrder(OrderModel orderModel)
+        {
+            orderModel.IdOrder = Guid.NewGuid();
+            dbContext.Orders.Add(MapModelToDbObject(orderModel));
+            dbContext.SaveChanges();
+        }
+
+        public void UpdateOrder(OrderModel orderModel)
+        {
+            Order order = dbContext.Orders.FirstOrDefault(x => x.IdOrder == orderModel.IdOrder);
+            if (order != null)
+            {
+                order.IdOrder = orderModel.IdOrder;
+                order.IdUser = orderModel.IdUser;
+                order.FirstName = orderModel.FirstName;
+                order.LastName = orderModel.LastName;
+                order.Address = orderModel.Address;
+                order.OrderDate = orderModel.OrderDate;
+            }
+            dbContext.SaveChanges();
+        }
+
+        public void DeleteOrder(Guid id)
+        {
+            var existingOrder = dbContext.Orders.FirstOrDefault(c => c.IdOrder == id);
+            if (existingOrder != null)
+            {
+                dbContext.Orders.Remove(existingOrder);
+
+            }
+            dbContext.SaveChanges();
+        }
+
+        private OrderModel MapDbObjectToModel(Order dbOrder)
+        {
+            OrderModel ordermodel = new OrderModel();
+            if (dbOrder != null)
+            {
+                ordermodel.IdOrder = dbOrder.IdOrder;
+                ordermodel.IdUser = dbOrder.IdUser;
+                ordermodel.FirstName = dbOrder.FirstName;
+                ordermodel.LastName = dbOrder.LastName;
+                ordermodel.Address = dbOrder.Address;
+                ordermodel.OrderDate = dbOrder.OrderDate;
+            }
+            return ordermodel;
+        }
+
+        private Order MapModelToDbObject(OrderModel orderModel)
+        {
+
+            Order dbOrder = new Order();
+            if (dbOrder != null)
+            {
+                dbOrder.IdOrder = orderModel.IdOrder;
+                dbOrder.IdUser = orderModel.IdUser;
+                dbOrder.FirstName = orderModel.FirstName;
+                dbOrder.LastName = orderModel.LastName;
+                dbOrder.Address = orderModel.Address;
+                dbOrder.OrderDate = orderModel.OrderDate;
+            }
+            return dbOrder;
+        }
 
 
     }
